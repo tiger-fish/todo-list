455be951b8728c9a6e620f9262d5d05fed23ad00
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-08 15:44:09 -0400

    updated ver. 3 and place in index

--- versions/todo-v3.js
+++ versions/todo-v3.js
@@ -1,17 +1,60 @@
 // 1. it should store the todos array on an object
-let todos = {
-  todolist: ["item 1", "item 2", "item 3"],
+let todoList = {
+  todos: ["item 1", "item 2", "item 3"],
 };
 
-// 2. it should have a displayTodo method
-let todos = {
-  todolist: ["item 1", "item 2", "item 3"],
-  displayTodo: function () {
-    console.log("My todos:", this.todolist);
+// 2. it should have a displayTodos method
+let todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function () {
+    console.log("My todos:", this.todos);
   },
 };
+
 // 3. it should have an addTodo method
+let todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function () {
+    console.log("My todos:", this.todos);
+  },
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+};
 
 // 4. it should have a changeTodo method
+let todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function () {
+    console.log("My todos:", this.todos);
+  },
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function (position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+};
 
 // 5. it should have a deleteTodo method
+let todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function () {
+    console.log("My todos:", this.todos);
+  },
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function (position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+};
