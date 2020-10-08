a131d800b22820a5431531dbadde6814c2e49775
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-08 16:42:58 -0400

    created practice.js

---
+++ practice.js
@@ -0,0 +1,31 @@
+// 1. todoList.addTodo should add objects
+
+// 2. todoList.changeTodo should change the todoText property
+
+// 3. todoList.toggleCompleted should change the completed propery
+
+let todoList = {
+  todos=[], 
+  displayTodo: function() {
+    console.log('My Todos', todos);
+  }, 
+  addTodo: function() {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    })
+    this.displayTodo();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodo();
+  },
+  deleteTodo (position) {
+    this.todos.splice(position, 1);
+    this.displayTodo();
+  }, 
+  toggleCompleted: function () {
+    let todo = this.todos[position]; 
+    todo = !completed;
+  }
+}
