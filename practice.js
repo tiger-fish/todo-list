587cb9c99906139e2891caca65cc833b8a02a674
tiger-fish <25111698+tiger+fish@users.noreply.github.com>
2020-10-11 22:49:36 -0400

    updated version 7

--- practice.js
+++ practice.js
@@ -2,28 +2,27 @@ let todoList = {
   todos: [],
   displayTodos: function () {
     if (this.todos.length === 0) {
-      console.log("Your todo list is empty.");
+      console.log("Your todo list is empty");
     } else {
       console.log("My Todos:");
-      for (let i = 0; i < this.todos.length; i++) {
+      for (i = 0; i < this.todos.length; i++) {
         if (this.todos[i].completed === true) {
-          console.log("(x)", this.todos[i].todoText);
+          console.log("(x)", this.todos[i].textTodo);
         } else {
-          console.log("( )", this.todos[i].todoText);
+          console.log("( )", this.todos[i].textTodo);
         }
       }
     }
   },
-  addTodo: function (todoText) {
+  addTodo: function (textTodo) {
     this.todos.push({
-      todoText: todoText,
+      textTodo: textTodo,
       completed: false,
     });
     this.displayTodos();
   },
-  changeTodo: function (position, todoText) {
-    // this.todos[position] = newValue;
-    this.todos[position].todoText = todoText;
+  changeTodo: function (position, textTodo) {
+    this.todos[position].textTodo = textTodo;
     this.displayTodos();
   },
   deleteTodo: function (position) {
@@ -36,16 +35,14 @@ let todoList = {
     this.displayTodos();
   },
   toggleAll: function () {
-    // count totals and completed
     let totalTodos = this.todos.length;
     let completedTodos = 0;
-    ``;
+
     for (i = 0; i < totalTodos; i++) {
       if (this.todos[i].completed === true) {
         completedTodos++;
       }
     }
-    // if all completed is true, turn false
     if (completedTodos === totalTodos) {
       for (i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
@@ -57,17 +54,24 @@ let todoList = {
     }
     this.displayTodos();
   },
+  deleteAll: function () {
+    this.todos.splice(0, this.todos.length);
+    this.displayTodos();
+  },
 };
 
-// 1. display-todos button should run display todos
 let displayTodosBtn = document.getElementById("display-todos");
 let toggleAllBtn = document.getElementById("toggle-all");
+let deleteAllBtn = document.getElementById("delete-all");
 
 displayTodosBtn.addEventListener("click", function () {
   todoList.displayTodos();
 });
 
-// 2. toggle-all button should run toggle all
 toggleAllBtn.addEventListener("click", function () {
   todoList.toggleAll();
+});
+
+deleteAllBtn.addEventListener("click", function () {
+  todoList.deleteAll();
 });
